9795bc6272d0e65c0ccef5b17a9cc70f5b74977e
OOHYEE <azss0470@gmail.com>
2021-04-27 23:55:01 +0900

    filter 메서드 정리

---
+++ Sytax.js/filter.js
@@ -0,0 +1,25 @@
+//  배열 filter Method 정리
+
+const array = [1, 2, 4];
+
+const example_1 = array.filter(i => i >= 2);
+console.log(example_1);
+
+//  1. filter 메서드는 콜백함수를 받아온다
+//  2. 조건이 참인 요소들만 모아 새로운 배열로 반환.
+
+// i => i>=2 해당조건을 만족하는 i만을 모아 새로운 배열로 반환!
+
+const example_2 = array.filter(i => {
+    if (i >= 4) {
+        return true;
+    }
+})
+console.log(example_2);
+
+const example_3 = array.filter(function (i) {
+    if (i > 0) {
+        return true;
+    }
+})
+console.log(example_3);
