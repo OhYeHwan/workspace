f2755825d4e391d43c8eb2d9f8403e9a99eea19e
OOHYEE <azss0470@gmail.com>
2021-05-06 16:57:19 +0900

    210506 - inject아이디어 생각

--- KDB_V1/src/components/EKDB/EKDBCardListContainer.js
+++ KDB_V1/src/components/EKDB/EKDBCardListContainer.js
@@ -5,6 +5,7 @@ import { Grid } from '@material-ui/core';
 import autobind from 'autobind-decorator';
 
 @inject("ekdbStore")
+@inject("ukStore")
 @observer
 @autobind
 class EKDBCardListContainer extends Component {
