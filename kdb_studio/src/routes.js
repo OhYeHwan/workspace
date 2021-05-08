457b6204888f505d816ea0536889bfd158b935a1
OOHYEE <azss0470@gmail.com>
2021-05-08 11:07:38 +0900

    210508 - routes.js파일에 UKLayout연결

--- kdb_studio/src/routes.js
+++ kdb_studio/src/routes.js
@@ -1,11 +1,18 @@
 import EKDBLayout from './layouts/EKDB/EKDBLayout';
+import UKLayout from './layouts/UK/UKLayout';
 import EKDB from './pages/EKDB';
+import UK from './pages/UK';
 
 const routes = [
   {
     path: '/ekdb',
     element: <EKDBLayout />,
     children: [{ path: '/', element: <EKDB /> }],
+  },
+  {
+    path: '/uk',
+    element: <UKLayout />,
+    children: [{ path: '/', element: <UK /> }],
   },
 ];
 
