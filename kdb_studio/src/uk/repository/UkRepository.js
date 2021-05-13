8da5a7821801c08f4524f74973ea97e1ff586e4e
OOHYEE <azss0470@gmail.com>
2021-05-13 10:53:39 +0900

    210513 - 전체 UK 조회 함수 구현(axios_GET)

--- kdb_studio/src/uk/repository/UkRepository.js
+++ kdb_studio/src/uk/repository/UkRepository.js
@@ -1,5 +1,12 @@
 import axios from 'axios';
 
-class UkRepository {}
+class UkRepository {
+  // 전체 UK 조회
+  funcGet(id) {
+    return axios
+      .get(`http://192.168.156.18:3009/EKDB/${id}/UK_LIST`)
+      .then(response => console.log(response));
+  }
+}
 
 export default new UkRepository();
