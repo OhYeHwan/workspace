343e1353424b97b81384e6c868c0fda541aeb75b
OOHYEE <azss0470@gmail.com>
2021-05-13 10:56:22 +0900

    210513 - UK 생성 함수 구현 (axios_POST)

--- kdb_studio/src/uk/repository/UkRepository.js
+++ kdb_studio/src/uk/repository/UkRepository.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
 
 class UkRepository {
+  funcInsert(id, data) {
+    return axios
+      .post(`http://192.168.156.18:3009/EKDB/${id}`, data)
+      .then(response => response);
+  }
+
   // 전체 UK 조회
   funcGet(id) {
     return axios
       .get(`http://192.168.156.18:3009/EKDB/${id}/UK_LIST`)
-      .then(response => console.log(response));
+      .then(response => response);
   }
 }
 
