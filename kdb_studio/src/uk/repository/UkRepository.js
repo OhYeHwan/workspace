981059b32500ff8cae074951e987319de1838451
OOHYEE <azss0470@gmail.com>
2021-05-13 11:02:01 +0900

    210513 - UK 삭제 함수 구현 (axios_DELETE)

--- kdb_studio/src/uk/repository/UkRepository.js
+++ kdb_studio/src/uk/repository/UkRepository.js
@@ -1,16 +1,24 @@
 import axios from 'axios';
 
 class UkRepository {
-  funcInsert(id, data) {
+  // 새로운 UK 생성
+  funcInsert(DBid, data) {
     return axios
-      .post(`http://192.168.156.18:3009/EKDB/${id}`, data)
+      .post(`http://192.168.156.18:3009/EKDB/${DBid}`, data)
       .then(response => response);
   }
 
   // 전체 UK 조회
-  funcGet(id) {
+  funcGet(DBid) {
     return axios
-      .get(`http://192.168.156.18:3009/EKDB/${id}/UK_LIST`)
+      .get(`http://192.168.156.18:3009/EKDB/${DBid}/UK_LIST`)
+      .then(response => response);
+  }
+
+  // UK 삭제
+  funcDel(DBid, name) {
+    return axios
+      .delete(`http://192.168.156.18:3009/EKDB/${DBid}/UK_LIST/${name}`)
       .then(response => response);
   }
 }
